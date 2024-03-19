2b377a186dad2c771bcd223515cd11938b4e0901
Combro-Kim <rlawog@nate.com>
2024-03-19 17:38:26 +0900

    lv3 complete

--- app/src/main/java/com/example/nbc_base/HomeActivity.kt
+++ app/src/main/java/com/example/nbc_base/HomeActivity.kt
@@ -1,10 +1,17 @@
 package com.example.nbc_base
 
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import android.widget.TextView
 
+//HomeActivity를 생성해 주세요.
+//SignInActivity에서 받은 extra data(아이디)를 화면에 표시합니다.
+//ImageView, TextView 외에 각종 Widget을 활용해 자유롭게 화면을 디자인 해주세요.
+//이름, 나이, MBTI 등 자기소개등이 들어가는 위젯을 자유롭게 디자인해주세요.
+//종료 버튼이 눌리면 SignInActivity로 이동하도록 구현합니다. (finish 활용)
 
 class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -13,12 +20,32 @@ class HomeActivity : AppCompatActivity() {
 
         takeData()
 
+        backToMain()
+
     }
 
+    private fun backToMain() {
+        val endButton = findViewById<Button>(R.id.endButton)
+        endButton.setOnClickListener {
+            finish()
+        }
+    }
+
+
+    @SuppressLint("SetTextI18n")
     private fun takeData() {
+        val showId = findViewById<TextView>(R.id.userId)
+        val showName = findViewById<TextView>(R.id.userName)
+        val showAge = findViewById<TextView>(R.id.userAge)
+        val showMBTI = findViewById<TextView>(R.id.userMBTI)
+
         val idData = intent.getStringExtra("dataFromSignInId")
-        val showId = findViewById<TextView>(R.id.takeIdTextView)
-        showId.text = idData
+
+        showId.text = "${getString(R.string.id)} : $idData"
+        showName.text = "${getString(R.string.name)} : 스마일"
+        showAge.text = "${getString(R.string.age)} : 101"
+        showMBTI.text = "MBTI : INFP"
+
     }
 
 }
--- app/src/main/java/com/example/nbc_base/SignUpActivity.kt
+++ app/src/main/java/com/example/nbc_base/SignUpActivity.kt
@@ -1,20 +1,13 @@
 package com.example.nbc_base
 
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
 
-
-//SignpActivity를 생성해 주세요.
-//타이틀 이미지는 원하는 이미지로 넣어주세요.
-//이름, 아이디, 비밀번호 모두 입력 되었을 때만 회원가입 버튼이 눌리도록 구현합니다.
-//셋 중 하나라도 비어있으면 “입력되지 않은 정보가 있습니다” 라는
-//토스트 메세지를 출력하도록 구현합니다.
-//비밀번호 EditText는 입력 내용이 가려져야 합니다.(●●● 처리)
-//회원가입 버튼이 눌리면 SignInActivity로 이동하도록 구현합니다. (finish 활용)
 
 class SignUpActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -38,6 +31,8 @@ class SignUpActivity : AppCompatActivity() {
                 Toast.makeText(this, "입력되지 않은 정보가 있습니다.", Toast.LENGTH_SHORT).show()
                 return@setOnClickListener
             }
+            val intent = Intent(this,HomeActivity::class.java)
+            intent.putExtra("dataFromSignUpName",name)
 
             Toast.makeText(this, "회원가입에 성공하셨습니다.", Toast.LENGTH_SHORT).show()
             finish()
